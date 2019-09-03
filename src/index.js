cccc3bb93f84e76e1abbd460114afdb5bcf8770c
Risa <42893634+HopeEternal@users.noreply.github.com>
2019-09-03 18:08:06 +0100

    Hide percent off when percentage is 0 or less

--- src/index.js
+++ src/index.js
@@ -7,3 +7,33 @@ import 'bootstrap';
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
 });
+
+
+// Detect for >0 in Percent-off bubbles and hide them
+
+function hideZeroPercent() {
+  let percentOff = Array.from(document.getElementsByClassName('percent-off-number'));
+
+  percentOff.forEach(checkNum);
+
+    function checkNum(item, index) {
+      let num = Array.from(item.innerHTML);
+
+      if (num[0] <= 0 ) {
+        let el = percentOff[index].parentNode.parentNode;
+        el.classList.add("d-none");
+      }
+  }
+}
+
+hideZeroPercent();
+
+
+
+
+
+
+
+
+
+
